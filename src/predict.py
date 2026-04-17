ecc5ef416c7198e28f5adb4e32d55fc4d4a79fe7
Renan <trevelimrenan@gmail.com>
2026-04-17 14:57:13 -0300

    alterando modelo e eda

--- src/predict.py
+++ src/predict.py
@@ -5,11 +5,15 @@ COL_ENTRADA = joblib.load("models/colunas_entrada.pkl")
 COL_MODELO = joblib.load("models/colunas_modelo.pkl")
 
 preprocessamento = joblib.load("models/pre_processamento.pkl")
-modelo = joblib.load("models/modelo_regressao_linear.pkl")
+modelo = joblib.load("models/modelo_gradiente_boosting.pkl")
 
 def predict(dados):
     
     df = pd.DataFrame(dados)
+
+    df['entrega_problematica'] = ((df['delivery_delay_days'] > 0) & (df['delivery_attempts'] > 1)).astype(int)
+    df['cliente_muito_acionado'] = (df['customer_service_contacts'] >= 3).astype(int)
+    
     df = df[COL_ENTRADA]
 
     df_transformado = preprocessamento.transform(df)
